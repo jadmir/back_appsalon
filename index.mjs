2267024897fb643ba10e1fe5033de8e91b152142
Jadmir <jadmir1111@gmail.com>
2023-10-05 11:32:42 -0500

    first commit

---
+++ index.mjs
@@ -0,0 +1,16 @@
+import express from 'express'
+import servicesRoutes from './routes/servicesRoutes.js'
+
+//configurar app
+const app = express()
+
+//definir una ruta
+app.use('/services', servicesRoutes)
+
+//definir puert
+const PORT = process.env.PORT || 4000
+
+//arrancar la app
+app.listen(PORT, () => {
+  console.log('El puerto se esta ejecutando en el puerto:', PORT )
+})
