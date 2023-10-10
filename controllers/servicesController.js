907e5b55d2267598b66996277f8827cbccfe80b9
Jadmir <jadmir1111@gmail.com>
2023-10-10 12:05:52 -0500

    utils

--- controllers/servicesController.js
+++ controllers/servicesController.js
@@ -1,10 +1,26 @@
+import mongoose from 'mongoose'
 import { services } from '../data/beautyServices.js'
+import Services from '../models/Services.js'
+import { validateObjectId, handleNotFoundError } from '../utils/index.js'
 
 const createService = async (req, res) => {
   if(Object.values(req.body).includes('')) {
-    res.json({
-      msq:'Todos los campos son obligatorios '
+    const error = new Error('Todos los campos son obligatorios ')
+
+    return res.status(400).json({
+      msq: error.message
     })
+  }
+
+  try {
+    const service = new Services(req.body)
+    await service.save()
+
+    res.json({
+      msq: 'El servicio se creo correctamente'
+    })
+  } catch (error) {
+    console.log(error)
   }
 }
 
@@ -12,7 +28,71 @@ const getServices = (req, res) => {
   res.send(services)
 }
 
+const getServiceById = async (req, res) => {
+  const { id } = req.params
+  //validar un object id
+  if(validateObjectId(id, res)) return
+
+  //validar que exista
+  const service = await Services.findById(id)
+  if(!service) {
+    return handleNotFoundError('El Servicio no existe', res)
+  }
+
+  //mostrar el servicio
+  res.json(service)
+}
+
+const updateService = async (req, res) => {
+  const { id } = req.params
+  //validar un object id
+  if(validateObjectId(id, res)) return
+
+  //validar que exista
+  const service = await Services.findById(id)
+  if(!service) {
+    return handleNotFoundError('El Servicio no existe', res)
+  }
+
+  //Escribimos en el objeto los valores nuevos
+  service.name = req.body.name || service.name
+  service.price = req.body.price || service.price
+
+  try {
+    await service.save()
+    res.json({
+      msg: 'El servicio se actualizo correctamente'
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const deleteService = async (req, res) => {
+  const { id } = req.params
+  //validar un object id
+  if(validateObjectId(id, res)) return
+
+  //validar que exista
+  const service = await Services.findById(id)
+  if(!service) {
+    return handleNotFoundError('El Servicio no existe', res)
+  }
+
+  try {
+    await service.deleteOne()
+    res.json({
+      msg: 'El servicio se elimino correctamente'
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 export {
   createService,
-  getServices
+  getServices,
+  getServiceById,
+  updateService,
+  deleteService
 }
--- routes/servicesRoutes.js
+++ routes/servicesRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express'
-import { createService, getServices } from '../controllers/servicesController.js'
+import { createService, getServices, getServiceById, updateService, deleteService } from '../controllers/servicesController.js'
 
 const router = express.Router()
 
-router.post('/', createService)
-router.get('/', getServices)
+router.route('/')
+    .post(createService)
+    .get(getServices)
+
+router.get('/:id', getServiceById)
+router.put('/:id', updateService)
+router.delete('/:id', deleteService)
 
 export default router
